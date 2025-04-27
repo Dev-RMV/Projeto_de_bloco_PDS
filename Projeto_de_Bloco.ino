8ba09aaa6caf06adcfb4dfb79ab919bd3b2b7671
Dev-RMV <rodrigovianna2@gmail.com>
2025-04-27 04:11:34 -0300

    Esboço do código não testado.
    Provavelmente o "println" não deve funcionar, e o cursor terá que ser movido para pular de linha ao mostrar os dados no display.

--- Projeto_de_Bloco.ino
+++ Projeto_de_Bloco.ino
@@ -5,12 +5,19 @@
 #define SCREEEN_HEIGHT 64
 #define OLED_ADDR 0x3c
 Adafruit_SSD1306 display(SCREEEN_WIDTH,SCREEEN_HEIGHT,&Wire,-1);
+
+#define SQRT_2  1.4142
+#define OFFSET 512.0 //2,5V aproximadamente
+#define Pi 3.1416
+
+int leitura = -1;
+int max = -1;
+int min = 1024;
+
 void setup() {
   // put your setup code here, to run once:  
   Serial.begin(9600);
-  
-  
-
+  pinMode(A0,INPUT);
 }
 
 void loop() {
@@ -20,14 +27,52 @@ void loop() {
     while(1){
       Serial.println("Nao foi possivel inicializar p display");
       delay(500);
-    }
-  
+    }  
   }
+
+  leitura = -1;
+  max = -1;
+  min = 1024;
+  for (int i=0 ; i<999 ; i++){
+    leitura = analogRead(A0);
+    if (leitura > max){
+      max = leitura;
+    };
+    if (leitura < min){
+      min = leitura;
+    };
+    delay(1);
+  };
+  //O OFFSET faz a onda ter amplitude de 0-2.5V, daí a multiplicação por 2.
+  //Isso se dá porque a saída do ZMPT é analógica de 0-5V, que com o OFFSET representa uma onda AC de amplitude máxima de 2.5V
+  float Vp = (2.0 * float(max-OFFSET)) * (250.0 / 1023.0);
+  float RMS = Vp / SQRT_2;
+  float Media = (2.0/Pi) * Vp;
+  float Vmin = (2.0 * float(min-OFFSET)) * (250.0 / 1023.0);
+  float Vpp = Vp - Vmin;
+
   display.clearDisplay();
   display.setTextSize(2);
-  display.setTextColor(SSD1306_WHIE);
+  display.setTextColor(SSD1306_WHITE);
   display.setCursor(10,25);
-  display.print("Funcionou?");
+  display.print("Vp: ");
+  display.print(Vp);
+  display.println("V");
+  display.print("Vpp: ");
+  display.print(Vpp);
+  display.println("V");
+  display.print("RMS: ");
+  display.print(RMS);
+  display.println("V");
+  display.print("Media: ");
+  display.print(Media);
+  display.println("V");
+  display.print("Vmin: ");
+  display.print(Vmin);
+  display.println("V");
+
+
+
   display.display();
   
 }
